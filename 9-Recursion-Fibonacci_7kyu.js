2712595397def7b2583f2b7bf4e0be3b32983bf1
yatoogamii <bracaval.elias.pro@gmail.com>
2019-03-21 00:01:57 +0100

    exercice Codewars Recursion-Fibonacci_7kyu

---
+++ 9-Recursion-Fibonacci_7kyu.js
@@ -0,0 +1,15 @@
+// In mathematical terms, the sequence f(n) of fibonacci numbers is defined by the recurrence relation
+
+// f(n) = f(n-1) + f(n-2)
+
+// with seed values
+
+// f(1) = 1 and f(2) = 1
+
+// #Your task
+
+// You have to create the function fibonacci that receives n and returns f(n). You have to use recursion.
+
+let fibonacci = n => {
+  return n <= 1 ? n : fibonacci(n - 1) + fibonacci(n - 2);
+};
